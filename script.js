75d17e02c70c1229caa1a5649818cc050915f5d4
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-12-01 20:47:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,33 +17,20 @@ window.onload = () => {
 
 function renderPlaces() {
    let scene = document.querySelector('a-scene');
-   let lat = [-7.289210000000001,
-            -7.289070000000001,
-            -7.2889800000000005,
-            -7.2889800000000005,
-            -7.289020000000001,
-            -7.289040000000001,
-            -7.289090000000001,
-            -7.289650000000001,
-            -7.28997]
-    let lon = [112.79693,
-            112.79690000000001,
-            112.79687000000001,
-            112.79683000000001,
-            112.79648,
-            112.79631,
-            112.79598000000001,
-            112.79606000000001,
-            112.79610000000001
+   let lat = [ -7.289123,
+              -7.289123]
+    let lon = [ 112.796941,
+               112.796901
         ]
 
-   for (let i = 0; i < lat.length; i++) {
-    let latitude = lat[i];
-    let longitude = lon[i];
+   
+    // let latitude = lat[i];
+    // let longitude = lon[i];
 
     let model = document.createElement('a-entity');
-    model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    model.setAttribute('gps-new-entity-place', `latitude: -7.289123; longitude: 112.796941;`);
     // model.setAttribute('material', { color: 'blue' } );
+    model.setAttribute('id', 'target')
     model.setAttribute('look-at', '[gps-new-camera]')
     model.setAttribute('gltf-model', '#animated-asset');
     model.setAttribute('animation-mixer', 'loop: repeat');
@@ -54,5 +41,20 @@ function renderPlaces() {
     });
 
     scene.appendChild(model);
-   }
+
+    let model = document.createElement('a-entity');
+    model.setAttribute('gps-new-entity-place', `latitude: -7.289123; longitude: 112.796901;`);
+    // model.setAttribute('material', { color: 'blue' } );
+    model.setAttribute('id', 'target1')
+    model.setAttribute('look-at', '#target')
+    model.setAttribute('gltf-model', '#animated-asset');
+    model.setAttribute('animation-mixer', 'loop: repeat');
+    model.setAttribute('scale', '0.5 0.5 0.5');
+
+    model.addEventListener('loaded', () => {
+        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+    });
+
+    scene.appendChild(model);
+   
 }
